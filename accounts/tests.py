9b39f0dd2c1900bbae9e3e816cf7a9d74e6143f9
Apfirebolt <aspper20@gmail.com>
2023-10-30 08:01:59 +0530

    added sample test cases

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from accounts.models import CustomUser
 
-# Create your tests here.
+
+class AccountsTestCase(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpass'
+        )
+
+    def test_login(self):
+        response = self.client.post(reverse('accounts:login'), {
+            'username': 'testuser@example.com',
+            'password': 'testpass'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('home'))
+
+    def test_register(self):
+        response = self.client.post(reverse('accounts:register'), {
+            'username': 'newuser',
+            'email': 'newuser@example.com',
+            'password1': 'newpass',
+            'password2': 'newpass'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(CustomUser.objects.filter(email='testuser@example.com').exists())
